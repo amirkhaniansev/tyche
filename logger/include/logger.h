06a30e0a714c1756d1246dc1548d97ca1ee7ecee
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-12-31 04:35:47 +0400

    fixed license note in logger

--- logger/include/logger.h
+++ logger/include/logger.h
@@ -1,6 +1,6 @@
 /*
  * GNU General Public License Version 3.0, 29 June 2007
- * Header file of stack.
+ * Header file of logger.
  * Copyright (C) 2018  Sevak Amirkhanian
  * Email: amirkhanyan.sevak@gmail.com
  * For full notice please see https://github.com/amirkhaniansev/tyche/tree/master/LICENSE.
