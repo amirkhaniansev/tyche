7fc3906ea7454510ff18aedaa0ab17adb42830a2
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2018-12-31 04:33:14 +0400

    added logger module

---
+++ logger/include/logger.h
@@ -0,0 +1,15 @@
+/*
+ * GNU General Public License Version 3.0, 29 June 2007
+ * Header file of stack.
+ * Copyright (C) 2018  Sevak Amirkhanian
+ * Email: amirkhanyan.sevak@gmail.com
+ * For full notice please see https://github.com/amirkhaniansev/tyche/tree/master/LICENSE.
+ */
+
+
+#ifndef __LOGGER_H_
+#define __LOGGER_H_
+
+
+
+#endif
---
+++ logger/src/logger.c
@@ -0,0 +1,9 @@
+/*
+ * GNU General Public License Version 3.0, 29 June 2007
+ * Source file of logger.
+ * Copyright (C) 2018  Sevak Amirkhanian
+ * Email: amirkhanyan.sevak@gmail.com
+ * For full notice please see https://github.com/amirkhaniansev/tyche/tree/master/LICENSE.
+ */
+
+#include "../include/logger.h"
