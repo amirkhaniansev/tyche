cb25d687b3721d78ed0c4f16a5235fe3bb65ac21
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-01-02 23:50:25 +0400

    added some new functionality to string_t

--- core/include/string_t.h
+++ core/include/string_t.h
@@ -11,12 +11,15 @@
 
 #include <stdbool.h>
 
-#define STRING_IS_NULL 					0x200
-#define STRING_OUT_OF_RANGE 			0x201
-#define STRING_BASE_IS_NULL 			0x202
-#define STRING_CMP_LEFT_IS_NULL 		0x203
-#define STRING_CMP_RIGHT_IS_NULL		0x204
-#define STRING_ALLOCATION_ERROR 		0x205
+#define STRING_IS_NULL 						0x200
+#define STRING_OUT_OF_RANGE 				0x201
+#define STRING_BASE_IS_NULL 				0x202
+#define STRING_CMP_LEFT_IS_NULL 			0x203
+#define STRING_CMP_RIGHT_IS_NULL			0x204
+#define STRING_ALLOCATION_ERROR 			0x205
+#define STRING_IS_ALREADY_INITIALIZED		0x206
+#define STRING_SYMBOL_NOT_FOUND				0x207
+#define STRING_INDEX_OF_CHAR_COUNT_ERROR	0x208
 
 /** structure for ASCII strings **/
 typedef struct string_structure
@@ -25,6 +28,19 @@ typedef struct string_structure
 	unsigned int _capacity;
 	char* _buffer;
 }string_t;
+
+/**
+ * string_t_init - initializes already created string. 
+ * 			This function is for scenarios when you want to create string
+ * 			object on stack
+ * @string - string
+ * 
+ * Returns 	0 								if initialization is successfull
+ * 			STRING_IS_NULL 					if the given string is null
+ * 			STRING_IS_ALREADY_INITIALIZED 	if the given string is already initialized
+ * 			STRING_ALLOCATION_ERROR			if allocation error happened	
+ */
+int string_t_init(string_t* string);
 
 /**
  * string_t_create - creates new string from the given c_string
@@ -109,6 +125,118 @@ bool string_t_contains(const string_t* string, const char symbol);
  * Returns the concatenation of 2 given strings if everything is OK,
  * 		otherwise the result will be NULL. 
  */
-string_t* string_t_concat(string_t* left, string_t* right);
+string_t* string_t_concat(string_t* left,string_t* right);
+
+/**
+ * string_t_concat_c - concatenates string and C-string
+ * @string - string
+ * @c_string - C-style null-terminated string
+ * 
+ * Returns the concatenation of 2 given strings if everything is OK,
+ * 		otherwise the result will be NULL. 
+ */
+string_t* string_t_concat_c(string_t* string,char* c_string);
+
+/**
+ * string_t_to_c - converts the given string to C-style null-terminated string
+ * @string - string
+ * Returns C-string if everything is OK otherwise the result is NULL.
+ */
+char* string_t_to_c(const string_t* string);
+
+/**
+ * string_t_starts_with_char - checks if the string starts with the given symbol
+ * @string - string
+ * @symbol - symbol
+ * 
+ * Returns true if string starts with the given symbol, false otherwise.
+ */
+bool string_t_starts_with_char(const string_t* string, const char symbol);
+
+/**
+ * string_t_starts_with_chars - checks if the string starts with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string starts with the given symbols, false otherwise
+ */
+bool string_t_starts_with_chars(const string_t* string, const char* symbols);
+
+/**
+ * string_t_starts_with_chars - checks if the string starts with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string starts with the given symbols, false otherwise
+ */
+bool string_t_starts_with_string(const string_t* string, const string_t* symbols);
+
+/**
+ * string_t_ends_with_char - checks if the string ends with the given symbol
+ * @string - string
+ * @symbol - symbol
+ * 
+ * Returns true if string ends with the given symbol, false otherwise.
+ */
+bool string_t_ends_with_char(const string_t* string, const char symbol);
+
+/**
+ * string_t_ends_with_chars - checks if the string ends with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string ends with the given symbols, false otherwise
+ */
+bool string_t_ends_with_chars(const string_t* string, const char* symbols);
+
+/**
+ * string_t_ends_with_chars - checks if the string ends with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string ends with the given symbols, false otherwise
+ */
+bool string_t_ends_with_string(const string_t* string, const string_t* symbols);
+
+/**
+ * string_t_equal - checks if 2 strings are equal
+ * @left - left string
+ * @right - right string
+ * 
+ * Returns true if 2 strings are equal and false otherwise.
+ */
+bool string_t_equal(const string_t* left, const string_t* right);
+
+/**
+ * string_t_equal - checks if 2 strings are equal
+ * @left - left string
+ * @right - right C-style null-terminated string
+ * 
+ * Returns true if 2 strings are equal and false otherwise.
+ */
+bool string_t_equal_c(const string_t* left, const string_t* right);
+
+/**
+ * string_t_index_of - returns the index of first occurrence of the given 
+ * 			symbol in the string
+ * @string - string
+ * @symbol - symbol
+ * 
+ * Returns 	STRING_IS_NULL			if string is null
+ * 			STRING_SYMBOL_NOT_FOUND	if symbol is not found
+ */
+unsigned int string_t_index_of(const string_t* string, const char symbol);
+
+/**
+ * string_t_index_of_any - returns the index of the first occurrence of the given
+ * 		symbols in the string. Note that ellipsis arguments must be chars.
+ * @string - string
+ * @amount - amount of chars
+ * @... - char arguments
+ * 
+ * Returns 	STRING_IS_NULL			if string is null
+ * 			STRING_SYMBOL_NOT_FOUND	if none of symbols is found
+ */
+unsigned int string_t_index_of_any(const string_t* string,unsigned int amount,...);
 
 #endif
--- core/src/string_t.c
+++ core/src/string_t.c
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 #include "../include/string_t.h"
 
@@ -66,6 +67,89 @@ static inline string_t* string_t_create(const char* c_string,
 	populate_string(string, c_string, len, 0);
 
 	return string;
+}
+
+static inline bool string_t_is_initialized(const string_t* string)
+{
+	if(string->_buffer != NULL &&
+	   string->_length > 0 &&
+	   string->_capacity >= 2 &&
+	   (string->_capacity & (string->_capacity - 1)) != 0)
+	   return true;
+	
+	return false;
+}
+
+static inline bool string_t_starts_with(const string_t* string, 
+		const char* symbols,
+		unsigned int len)
+{
+	if(string == NULL || string->_buffer == NULL)
+		return false;
+
+	if(len > string->_length)
+		return false;
+
+	unsigned int i = 0, j = 0;
+
+	while(i < len)
+		if(symbols[i++] != string->_buffer[j++])
+			return false;
+	
+	return true;
+}
+
+static inline bool string_t_ends_with(const string_t* string, 
+		const char* symbols,
+		unsigned int len)
+{
+	if(string == NULL || string->_buffer == NULL)
+		return false;
+
+	if(len > string->_length)
+		return false;
+
+	unsigned int i =  len - 1, j = string->_length - 1;
+
+	while(i)
+		if(symbols[i--] != string->_buffer[j--])
+			return false;	
+	
+	return true;
+}
+
+/**
+ * string_t_init - initializes already created string. 
+ * 			This function is for scenarios when you want to create string
+ * 			object on stack
+ * @string - string
+ * 
+ * Returns 	0 								if initialization is successfull
+ * 			STRING_IS_NULL 					if the given string is null
+ * 			STRING_IS_ALREADY_INITIALIZED 	if the given string is already initialized
+ * 			STRING_ALLOCATION_ERROR			if allocation error happened	
+ */
+int string_t_init(string_t* string)
+{
+	if(string == NULL)
+		return STRING_IS_NULL;
+	
+	if(string_t_is_initialized(string))
+		return STRING_IS_ALREADY_INITIALIZED;
+	
+	if(string->_buffer != NULL)
+		free(string->_buffer);
+
+	string->_length = 0;
+	string->_capacity = 2;
+	string->_buffer = malloc(string->_capacity * sizeof(char));
+
+	if(string->_buffer == NULL)
+		return STRING_ALLOCATION_ERROR;
+	
+	string->_buffer[0] = '\0';
+
+	return 0;
 }
 
 /**
@@ -245,4 +329,242 @@ string_t* string_t_concat(string_t* left, string_t* right)
 	populate_string(result, right->_buffer, result->_length, left->_length);
 
 	return result;
+}
+
+/**
+ * string_t_concat_c - concatenates string and C-string
+ * @string - string
+ * @c_string - C-style null-terminated string
+ * 
+ * Returns the concatenation of 2 given strings if everything is OK,
+ * 		otherwise the result will be NULL. 
+ */
+string_t* string_t_concat_c(string_t* string,char* c_string)
+{
+	if(string == NULL)
+		return string_t_create_c(c_string);
+
+	if(c_string == NULL)
+		return string;
+
+	unsigned int result_len = string->_length + strlen(c_string);
+
+	string_t* result = string_t_allocate(result_len, get_power_of_2(result_len));
+
+	populate_string(result, string->_buffer, string->_length, 0);
+	populate_string(result, c_string, result->_length, string->_length);
+
+	return result; 	
+}
+
+/**
+ * string_t_to_c - converts the given string to C-style null-terminated string
+ * @string - string
+ * Returns C-string if everything is OK otherwise the result is NULL.
+ */
+char* string_t_to_c(const string_t* string)
+{
+	if(string == NULL)
+		return NULL;
+
+	char* result = malloc((string->_length + 1) * sizeof(char));
+
+	if(result == NULL)
+		return NULL;
+
+	for(unsigned int i = 0; i < string->_length; i++)
+		result[i] = string->_buffer[i];
+	
+	result[string->_length] = '\0';
+
+	return result;
+}
+
+/**
+ * string_t_starts_with_char - checks if the string starts with the given symbol
+ * @string - string
+ * @symbol - symbol
+ * 
+ * Returns true if string starts with the given symbol, false otherwise.
+ */
+bool string_t_starts_with_char(const string_t* string, const char symbol)
+{
+	if(string == NULL || string->_buffer == NULL)
+		return false;
+
+	return string->_buffer[0] == symbol;
+}
+
+/**
+ * string_t_starts_with_chars - checks if the string starts with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string starts with the given symbols, false otherwise
+ */
+bool string_t_starts_with_chars(const string_t* string, const char* symbols)
+{
+	if(symbols == NULL)
+		return false;
+	
+	return string_t_starts_with(string, symbols, strlen(symbols));
+}
+
+/**
+ * string_t_starts_with_chars - checks if the string starts with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string starts with the given symbols, false otherwise
+ */
+bool string_t_starts_with_string(const string_t* string, const string_t* symbols)
+{
+	if(symbols == NULL || symbols->_buffer == NULL)
+		return false;
+	
+	return string_t_starts_with(string, symbols->_buffer, symbols->_length);
+}
+
+/**
+ * string_t_ends_with_char - checks if the string ends with the given symbol
+ * @string - string
+ * @symbol - symbol
+ * 
+ * Returns true if string ends with the given symbol, false otherwise.
+ */
+bool string_t_ends_with_char(const string_t* string, const char symbol)
+{
+	if(string == NULL || string->_buffer == NULL)
+		return false;
+	
+	return string->_buffer[string->_length-1] == symbol;
+}
+
+/**
+ * string_t_ends_with_chars - checks if the string ends with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string ends with the given symbols, false otherwise
+ */
+bool string_t_ends_with_chars(const string_t* string, const char* symbols)
+{
+	if(symbols == NULL)
+		return false;
+	
+	return string_t_ends_with(string, symbols, strlen(symbols));
+}
+
+/**
+ * string_t_ends_with_chars - checks if the string ends with the given symbols
+ * @string - string
+ * @symbols - symbols
+ * 
+ * Returns true if string ends with the given symbols, false otherwise
+ */
+bool string_t_ends_with_string(const string_t* string, const string_t* symbols)
+{
+	if(symbols == NULL || symbols->_buffer == NULL)
+		return false;
+
+	return string_t_ends_with(string, symbols->_buffer, symbols->_length);
+}
+
+/**
+ * string_t_equal - checks if 2 strings are equal
+ * @left - left string
+ * @right - right string
+ * 
+ * Returns true if 2 strings are equal and false otherwise.
+ */
+bool string_t_equal(const string_t* left, const string_t* right)
+{
+	return string_t_compare(left, right) == 0;
+}
+
+/**
+ * string_t_equal - checks if 2 strings are equal
+ * @left - left string
+ * @right - right C-style null-terminated string
+ * 
+ * Returns true if 2 strings are equal and false otherwise.
+ */
+bool string_t_equal_c(const string_t* left, const string_t* right)
+{
+	if(right == NULL)
+		return false;
+
+	return string_t_compare_c_string(left, right->_buffer) == 0;
+}
+
+/**
+ * string_t_index_of - returns the index of first occurrence of the given 
+ * 			symbol in the string
+ * @string - string
+ * @symbol - symbol
+ * 
+ * Returns 	STRING_IS_NULL			if string is null
+ * 			STRING_SYMBOL_NOT_FOUND	if symbol is not found
+ */
+unsigned int string_t_index_of(const string_t* string, const char symbol)
+{
+	if(string == NULL)
+		return STRING_IS_NULL;
+
+	if(string->_buffer == NULL)
+		return STRING_BASE_IS_NULL;
+	
+	for(unsigned int i = 0; i < string->_length; i++)
+		if(string->_buffer[i] == symbol)
+			return i;
+
+	return STRING_SYMBOL_NOT_FOUND;
+}
+
+/**
+ * string_t_index_of_any - returns the index of the first occurrence of the given
+ * 		symbols in the string. Note that ellipsis arguments must be chars.
+ * @string - string
+ * @amount - amount of chars
+ * @... - char arguments
+ * 
+ * Returns 	STRING_IS_NULL						if string is null
+ * 			STRING_SYMBOL_NOT_FOUND				if none of symbols is found
+ * 			STRING_BASE_IS_NULL					if buffer is null
+ *			STRING_INDEX_OF_CHAR_COUNT_ERROR 	if amount of char arguments is not positive
+ */
+unsigned int string_t_index_of_any(const string_t* string,unsigned int amount,...)
+{
+	if(string == NULL)
+		return STRING_IS_NULL;
+
+	if(string->_buffer == NULL)
+		return STRING_BASE_IS_NULL;
+
+	if(amount <= 0)
+		return STRING_INDEX_OF_CHAR_COUNT_ERROR;
+
+	va_list symbols;
+	va_start(symbols, amount);
+
+	unsigned int i = 0;
+	bool found = false;
+	char symbol;
+
+	while(amount-- && !found) {
+		symbol = va_arg(symbols, int);
+		for(i = 0; i < string->_length; i++) {
+			if(symbol == string->_buffer[i]) {
+				found = true;
+				break;
+			}
+		}
+	}
+
+	va_end(symbols);
+	
+	if(found)
+		return i;
+
+	return STRING_SYMBOL_NOT_FOUND;			
 }
