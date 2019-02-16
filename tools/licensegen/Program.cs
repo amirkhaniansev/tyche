0a4e962bbad7032d7d138240e0e2b1509c90c4c1
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-02-16 21:15:49 +0400

    fixed license generator tool new line issue

--- tools/licensegen/Program.cs
+++ tools/licensegen/Program.cs
@@ -89,7 +89,7 @@ namespace licensegen
 
                 fileName = Path.GetFileNameWithoutExtension(path);
                 licenseText = string.Format(license, fileName);
-                text = licenseText + "\r\n" +"\r\n" + text;
+                text = licenseText + "\r\n" + text;
                 File.WriteAllText(path, text);
                 Console.WriteLine("License is appended to {0}", path);
             }
