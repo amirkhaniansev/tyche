c6a5b2a9b72916018458999a5e1e1144f23a3717
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-02-23 22:06:26 +0400

    finised map file generator

--- tools/mapgen/Program.cs
+++ tools/mapgen/Program.cs
@@ -1,7 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+/**
+ * GNU General Public License Version 3.0, 29 June 2007
+ * Program
+ * Copyright (C) <2019>
+ *      Authors: <amirkhaniansev>  <amirkhanyan.sevak@gmail.com>
+ *               <DavidPetr>       <david.petrosyan11100@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Full notice : https://github.com/amirkhaniansev/tyche/tree/master/LICENSE
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+using System;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Collections.Generic;
+
+/*
+ * This tool is for fastening development process. 
+ * It is not optimized or the best solution for such tasks.
+ */
 
 namespace mapgen
 {
@@ -24,43 +51,77 @@ namespace mapgen
                 return;
             }
 
-            if (!Directory.Exists(xmlPath))
+            if (!File.Exists(xmlPath))
             {
                 Console.WriteLine("Invalid xml path.");
                 return;
             }
 
-            var paths = Directory.GetFiles(sqlPath, "*.sql", SearchOption.TopDirectoryOnly);
+            var paths = Directory.GetFiles(sqlPath, "*.sql", SearchOption.AllDirectories);
             var xmlBuilder = new StringBuilder();
             var parameters = new List<string>();
+            var script = new string[0];
             var spName = string.Empty;
             var opName = string.Empty;
-            var script = string.Empty;
-            var typeStart = 0;
-            var typeEnd = 0;
-            var pStart = 0;
+            var typeLine = string.Empty;
             var typeText = string.Empty;
             var type = string.Empty;
+            var count = 1;
 
-            xmlBuilder.Append("<?xml version=\"1.0\" encoding=\"utf - 8\" ?>\r\n");
+            xmlBuilder.Append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n");
             xmlBuilder.Append("<operations\r\n");
             xmlBuilder.Append("\t\t\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n");
-            xmlBuilder.Append("\t\t\txsi:noNamespaceSchemaLocation=\"scheme.xsd\"\r\n");
+            xmlBuilder.Append("\t\t\txsi:noNamespaceSchemaLocation=\"scheme.xsd\">\r\n\r\n");
 
             foreach (var path in paths)
             {
-                script = File.ReadAllText(path);
-
                 spName = Path.GetFileNameWithoutExtension(path);
                 opName = spName.Remove(0, 3);
+                script = File.ReadAllLines(path);
+                typeLine = script.First(str => str.StartsWith("/***"));
+                type = typeLine.Substring(4, typeLine.Length - 8)
+                    .Split(":")
+                    .Last()
+                    .Replace(" ","");
 
-                typeStart = script.IndexOf("/***") + 4;
-                typeEnd = script.IndexOf("***/") - 4;
-                typeText = script.Substring(typeStart, typeEnd - typeStart);
-                type = script.Split(new char[] { ':' }, StringSplitOptions.None)[1];
-                
-                // TODO
+                parameters = script.Where(str => str.StartsWith("\t@"))
+                    .Select(str => str.Split("\t")[1].Remove(0,1))
+                    .ToList();
+
+                xmlBuilder.AppendFormat("\t<!-- Operation Number : {0} -->\r\n", count++);
+                xmlBuilder.AppendFormat("\t<operation name=\"{0}\">\r\n", opName);
+                xmlBuilder.AppendFormat("\t\t<spName>{0}</spName>\r\n", spName);
+                xmlBuilder.Append("\t\t<parameters>\r\n");
+
+                // Be careful.
+                // Boilerplate is here.
+                if (parameters.Count == 1)
+                {
+                    xmlBuilder.Append("\t\t\t<parameter>\r\n");
+                    xmlBuilder.AppendFormat("\t\t\t\t<parameterName>primitive</parameterName>\r\n");
+                    xmlBuilder.AppendFormat("\t\t\t\t<spParameterName>{0}</spParameterName>\r\n",
+                        parameters.First());
+                    xmlBuilder.Append("\t\t\t</parameter>\r\n");
+                }
+                foreach (var parameter in parameters)
+                {
+                    xmlBuilder.Append("\t\t\t<parameter>\r\n");
+                    xmlBuilder.AppendFormat("\t\t\t\t<parameterName>{0}</parameterName>\r\n",
+                        parameter.First().ToString().ToUpper() + parameter.Substring(1));
+                    xmlBuilder.AppendFormat("\t\t\t\t<spParameterName>{0}</spParameterName>\r\n",
+                        parameter);
+                    xmlBuilder.Append("\t\t\t</parameter>\r\n");
+                }
+
+                xmlBuilder.Append("\t\t</parameters>\r\n");
+                xmlBuilder.AppendFormat("\t\t<returnDataType>{0}</returnDataType>\r\n", type);
+                xmlBuilder.Append("\t</operation>\r\n\r\n");
             }
+
+            xmlBuilder.Append("</operations>");
+
+            var xml = xmlBuilder.ToString();
+            File.WriteAllText(xmlPath, xml);
         }
     }
 }
