3a9b3dc41efa5bd4a3ea175b426d430fe05d8709
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-03-30 10:55:47 +0400

    added namespace generator

---
+++ tools/namespacegen/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace namespacegen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string invaliAmountOfArguments = "Invalid amount of arguments.";
+            const string directoryNotExist = "Directory does not exist.";
+            
+            if (args.Length < 2)
+            {
+                Console.WriteLine(invaliAmountOfArguments);
+                return;
+            }
+
+            var directory = args[0];
+            var topLevelNamespace = args[1];
+            var reserved = args.Skip(2);
+
+            if (!Directory.Exists(directory))
+            {
+                Console.WriteLine(directoryNotExist);
+                return;
+            }
+
+            const string extension = "*.cs";
+            const string usingText = "using";
+            const string namespaceText = "namespace";
+
+            var allPaths = Directory.GetFiles(directory, extension, SearchOption.AllDirectories);
+           
+            var lines = default(string[]);
+            var parts = default(string[]);
+            var actualNamespace = string.Empty;
+            var constructedLine = string.Empty;
+            var fileText = string.Empty;
+            var sb = new StringBuilder();
+
+            foreach (var path in allPaths)
+            {
+                lines = File.ReadAllLines(path);
+
+                foreach(var line in lines)
+                {
+                    constructedLine = line;
+
+                    if (line.StartsWith(usingText) || line.StartsWith(namespaceText))
+                    {
+                        parts = line.Split(" ");
+                        actualNamespace = parts[1];
+
+                        if (!reserved.Any(n => actualNamespace.StartsWith(n)))
+                        {
+                            actualNamespace = topLevelNamespace + "." + actualNamespace;
+                            constructedLine = parts[0] + " " + actualNamespace;
+                        }
+                    }
+
+                    sb.Append(constructedLine);
+                    sb.Append(Environment.NewLine);
+                }
+
+                fileText = sb.ToString();
+
+                File.WriteAllText(path, fileText);
+
+                sb.Clear();
+            }
+        }
+    }
+}
