eb23ff660156143e5e814f6a4da116e7656fdeda
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-03-30 19:36:32 +0400

    added data access layer for restrictions

---
+++ api/TycheDAL/DataAccess/RestrictionsDal.cs
@@ -0,0 +1,12 @@
+﻿using Tyche.TycheDAL.Context;
+
+namespace Tyche.TycheDAL.DataAccess
+{
+    public class RestrictionsDal : BaseDal
+    {
+        public RestrictionsDal(string connectionString, TycheContext context = null) 
+            : base(connectionString, context)
+        {
+        }
+    }
+}
