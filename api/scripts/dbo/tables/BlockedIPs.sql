002f913106aff6768ef45a90dfccc015c3de9c2b
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-03-30 19:03:10 +0400

    added some changes
    Now API solution cannot be built.

---
+++ api/scripts/dbo/tables/BlockedIPs.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[BlockedIPs]
+(
+	[Id]		INT				IDENTITY(1,1) NOT NULL,	
+	[IPAddress]	VARCHAR(255)	NOT NULL,
+	[StartDate]	DATETIME		NOT NULL,
+	[EndDate]	DATETIME		NOT NULL,
+	[Reason]	INT				NULL
+
+	
+)
