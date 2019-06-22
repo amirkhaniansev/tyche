bfdaeecc2e18fc37f124a6520fc68d6e479536d0
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-06-23 03:58:17 +0400

    added license notes in Model Generator

--- tools/modelgen/Queries/ProceduresParameters.sql
+++ tools/modelgen/Queries/ProceduresParameters.sql
@@ -1,4 +1,25 @@
-﻿SELECT	p.[parameter_id]	AS	[Id],
+/**
+ * GNU General Public License Version 3.0, 29 June 2007
+ * ProceduresParameters
+ * Copyright (C) <2019>
+ *      Authors: <amirkhaniansev>  <amirkhanyan.sevak@gmail.com>
+ *               <DavidPetr>       <david.petrosyan11100@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Full notice : https://github.com/amirkhaniansev/tyche/tree/master/LICENSE
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+SELECT	p.[parameter_id]	AS	[Id],
 	p.[object_id]		AS	[ProcedureId],
 	p.[name]		AS	[Name],
 	p.[system_type_id]	AS	[Type],
